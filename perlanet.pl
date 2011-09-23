350c044096ac6fca6ec27c44bf29abf9311bc733
WWW <www@int80.biz>
2011-09-22 23:55:50 -0700

    gimmie the works eh

--- perlanet.pl
+++ perlanet.pl
@@ -1,9 +1,45 @@
 #!/usr/bin/env perl
 
+package MSIN;
+
 use Moose;
-use Perlanet::Simple;
+use namespace::autoclean;
 
-my $perlanet = Perlanet::Simple->new_with_config('config.yml');
+use Carp;
+use YAML 'LoadFile';
+
+extends 'Perlanet';
+with qw(
+  Perlanet::Trait::Cache
+  Perlanet::Trait::OPML
+  Perlanet::Trait::Scrubber
+  Perlanet::Trait::Tidy
+  Perlanet::Trait::YAMLConfig
+  Perlanet::Trait::TemplateToolkit
+  Perlanet::Trait::FeedFile
+);
+
+around '_build_ua' => sub {
+  my $orig = shift;
+  my $self = shift;
+  my $ua = $self->$orig;
+  $ua->agent($self->agent) if $self->agent;
+  return $ua;
+};
+
+around clean_html => sub {
+    my $orig = shift;
+    my ($self, $html) = @_;
+
+    # hack to remove a particularly nasty piece of blogspot HTML
+    $html = $self->$orig($html);
+    $html =~ s|<div align="justify"></div>||g;
+
+    return $html;
+};
+
+my $perlanet = MSIN->new_with_config(
+    configfile => 'config.yml'
+);
+
 $perlanet->run;
-
-
